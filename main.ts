3bca842f1ad2fa181e46ada29101a11a4d126df4
senseiheather <heather.mckie@codeninjas.com>
2026-03-27 17:45:00 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
+/**
+ * For better readability, you should rewrite the function
+ * signature in a "natural language" way. You can refer to
+ * each variable in any order using the $VARIABLE syntax.
+ */
+
+//% color="#AA278D"
+namespace formatting {
+
+    //% block="fooooooo"
+    export function foo() {
+
+    }
+
+    //% block="testFunction does stuff = $text"
+    export function testFunction(text: string){
+        game.splash(text)
+    }
+    //% block="bar x = $x text = $text"
+    export function bar(x: number, text: string) {
+
+    }
+
+    //% block="shuffled text = $text x = $x"
+    export function shuffled(x: number, text: string) {
+
+    }
+}
 
