6a284721370f190b00267bfd3ff6f167303a481b
senseiheather <heather.mckie@codeninjas.com>
2026-03-31 15:29:17 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -13,15 +13,18 @@ namespace formatting {
 
     }
 
-    //% block="Find Average Number
-    export function avgNum(arr: array, speak: sprite){
-        effects.blizzard.startScreenEffect(1000)
+    //% block="testing" 
+    export function sortNums(avgArray: any[], spriteSpeaking: Sprite) {
+    if (avgArray.length > 0) {
+        averageAge = 0
+        for (let value of numList) {
+            averageAge += value
+            spriteSpeaking.sayText(convertToText(value), 500, true)
+            pause(500)
+        }
+        pause(2000)
     }
-
-    //% block="bar x = $x text = $text"
-    export function bar(x: number, text: string) {
-
-    }
+}
 
     //% block="shuffled text = $text x = $x"
     export function shuffled(x: number, text: string) {
