8999bc2be336aae4d16a6959e13373eb511ae626
senseiheather <heather.mckie@codeninjas.com>
2026-04-08 18:04:28 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -56,7 +56,7 @@ export function avgAgesortNums(avgArray: number[], spriteSpeaking: Sprite) {
     /*
     *@param numberArray
     */ 
-    //numberArray.shadow="variables_get" numberArray.delf="list" 
+    //% numberArray.shadow="variables_get" numberArray.delf="list" 
     //%block="Ask for age from $numberArray"
     export function askQuestion (numberArray: any[]) {
     list.push(game.askForNumber("How old are you?"))
