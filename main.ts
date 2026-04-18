893422c5c0661d4514c75d2413deb458251c7c40
senseiheather <heather.mckie@codeninjas.com>
2026-04-18 10:34:23 -0400

    Update main.ts
    testing new stuff with story

--- main.ts
+++ main.ts
@@ -61,5 +61,66 @@ export function avgAgesortNums(avgArray: number[], spriteSpeaking: Sprite) {
     export function askQuestion (numberArray: number[]) {
     list.push(game.askForNumber("How old are you?"), 2)
 }
-    
+
+
+    /*
+    *@param answerChoices
+    *@param answerCount
+    */ 
+    //% answerChoices.shadow="variables_get" answerChoices.delf="list" 
+    //% answerCount.shadow="variables_get" answerCount.delf="list"
+    //%block="Use %answerChoices to hold choices and %answerCount to count responses" 
+    export function answerQuestion (answerChoices: string[], answerCount: number[]) {
+    story.showPlayerChoices(answerChoices[0], answerChoices[1], answerChoices[2], answerChoices[3])
+    if (story.checkLastAnswer(answerChoices[0])) {
+        answerCount[0] = answerCount[0] + 1
+    } else if (story.checkLastAnswer(answerChoices[1])) {
+        answerCount[1] = answerCount[1] + 1
+    } else if (story.checkLastAnswer(answerChoices[2])) {
+        answerCount[2] = answerCount[2] + 1
+    } else if (story.checkLastAnswer(answerChoices[3])) {
+        answerCount[3] = answerCount[3] + 1
+    }
+}
+
+    /*
+    *@param answerList
+    *@param answerCounter
+    *@param fave
+    //% answerList.shadow="variables_get" answerList.delf="list" 
+    //% answerCounter.shadow="variables_get" answerCounter.delf="list" 
+    //% fave.shadow="variables_get" fave.delf="string" 
+    //%block="Use $answerList for choices, $answerCounter for votes, and $fave to find the favorite"  
+    */ 
+    export function checkFavorite (answerList: any[], answerCounter: any[], fave: string) {
+    biggestNum = 0
+    counter = 0
+    checkVal = 0
+    for (let value of answerCounter) {
+        if (value > biggestNum) {
+            biggestNum = value
+            checkVal = counter
+        }
+        counter += 1
+    }
+    game.showLongText("the best " + fave + " is " + answerList[checkVal], DialogLayout.Bottom)
+}
+    /*
+    *@param ans
+    *@param counter
+    //% ans.shadow="variables_list" ans.delf="answers" 
+    //% counter.shadow="variables_list" counter.delf="counter" 
+    */ 
+    export function createSurvey (ans: string[], counter: number[]) {
+    ans.shift()
+    counter.shift()
+    game.showLongText("You will ask someone about their favorite thing! Each answer will need to be a single word", DialogLayout.Center)
+    ques = game.askForString("What favorite thing should they list?")
+    for (let index = 0; index < 4; index++) {
+        answer1 = game.askForString("Add one choice for the player(s) to answer")
+        ans.push(answer1)
+        counter.push(0)
+    }
+    return ques
+}
 } 
